be137c0ea12ebbac7110dc2cb03b17e327600394
Johnathan <jocuog@gmail.com>
2022-04-25 08:53:42 -0600

    commit tree 4e6faaafde9685015a1af63310e856fe960178d7

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown (startNumber)
+    while startNumber > 0 
+    puts "#{startNumber} SECOND(S)!"
+    startNumber -= 1
+    end
+     "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (sleepFor)
+        sleep(sleepFor)
+    end
